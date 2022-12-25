51aea406622386aefacf8342e2d77a09a7e9d72d
Neha Yednurwar <yednurwarneha@gmail.com>
2022-12-25 18:43:41 +0530

    recent changes

--- Screens/ChatFeedDemo.js
+++ Screens/ChatFeedDemo.js
@@ -1,16 +1,16 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, TextInput } from 'react-native'
 import React from 'react'
 import { Feather } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
-
-
-
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 
 
 const FindScreen = () => {
   return (
+  <>
     <View style={{ display: 'flex', flexDirection: 'column' }}>
+     
       <View style={styles.header}>
         <TouchableOpacity>
           <View style={{ width: 30, height: 30, borderColor: '#4D4D4D', borderWidth: 1, marginLeft: 20, marginTop: 5 }}>
@@ -36,15 +36,19 @@ const FindScreen = () => {
         </View>
 
       </View>
-
-      <View style={{ borderBottomColor: 'gray', borderBottomWidth: 1 }}></View>
-
+    
       <View style={styles.external}>
-        <ScrollView style={styles.internal}>
+       <ScrollView>
+       <View style={styles.internal}>
           <View style={styles.chatBody}>
             <Image style={{ width: 35, height: 35, marginLeft: 15, borderRadius: 40, marginTop: 10 }} source={{ uri: 'https://images.unsplash.com/photo-1601288496920-b6154fe3626a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' }} />
             <View style={styles.chatReciever}>
-              <Text style={{ color: 'white' }}>heyyyaaa how are you doin?heyyyaaa how are you doin?heyyyaaa how are you doin?heyyyaaa how are you doin?heyyyaaa how are you doin?</Text>
+              <Text style={{ color: 'white' }}>heyyyaaa howwwwwwwwwwww are you doin?heyyyaaa how are you doin?heyyyaaa how are you doin?heyyyaaa how are you doin?heyyyaaa how are you doin?</Text>
+            </View>
+          </View>
+          <View style={styles.chatBodySend}>
+            <View style={styles.chatSender}>
+              <Text style={{ color: 'white' }}>heyyyaaaaaaaaaaaaaaaaaaaaaa how are you doin?</Text>
             </View>
           </View>
           <View style={styles.chatBodySend}>
@@ -91,12 +95,7 @@ const FindScreen = () => {
             <View style={styles.chatSender}>
               <Text style={{ color: 'white' }}>heyyyaaa how are you doin?</Text>
             </View>
-          </View>
-          <View style={styles.chatBodySend}>
-            <View style={styles.chatSender}>
-              <Text style={{ color: 'white' }}>heyyyaaa how are you doin?</Text>
-            </View>
-          </View>
+          </View> 
           <View style={styles.chatBodySend}>
             <View style={styles.chatSender}>
               <Text style={{ color: 'white' }}>heyyyaaa how are you doin?</Text>
@@ -113,11 +112,20 @@ const FindScreen = () => {
             </View>
           </View>
 
-        </ScrollView>
-
+        </View>
+        
+       </ScrollView>
+      <View style={{display:'flex', flexDirection:"row" ,justifyContent:'space-between', paddingHorizontal:15, paddingVertical:10,  borderColor:"gray", borderWidth:1, paddingTop:10, borderRadius:50, marginBottom:40}}>
+          <TextInput placeholder='Message' placeholderTextColor="gray"  style={{fontSize:20, paddingHorizontal:20, paddingBottom:5,}}         />
+          <MaterialCommunityIcons name="send-outline"  size={27} color="white" />
       </View>
-
+      </View>
+    
+      
+     
     </View>
+       </>
+   
   )
 }
 
@@ -133,16 +141,18 @@ const styles = StyleSheet.create({
 
   },
   external: {
-    height: '100%',
+    maxHeight:"90%",
     backgroundColor: 'black',
+  
 
   },
   internal: {
     backgroundColor: 'linear-gradient(360deg, #333333 0%, rgba(51, 51, 51, 0.3) 100%)',
-    height: '68%',
     margin: 15,
     borderRadius: 20,
-    
+    marginBottom:130,
+    paddingBottom:40,
+    height:"100%"
 
   },
   chatBody: {
@@ -150,6 +160,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     maxWidth:280,
+    
   },
   chatReciever: {
     backgroundColor: 'black',
@@ -157,8 +168,8 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: 30,
     borderTopRightRadius: 30,
     padding: 12,
-    marginLeft: 10
-
+    marginLeft: 10,
+   
   },
   chatBodySend: {
     display: 'flex',
@@ -166,6 +177,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingTop: 20,
     paddingLeft: 12,
+  
   },
   chatSender: {
     borderTopLeftRadius: 30,
@@ -174,7 +186,7 @@ const styles = StyleSheet.create({
     padding: 12,
     backgroundColor: '#0088CC',
     maxWidth:280,
-
+    
   }
 
 })
